b190fbfb516a825c00703fedebc2f76a45f7cd3e
Поликарпов Илья Николаевич <ipolikarpov@jfusion.ru>
2020-02-18 17:27:34 +0300

    consull retry demo

---
+++ spring-cloud-hozton-sr1/src/test/java/com/example/consul/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consul.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
